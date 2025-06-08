2c31577d8ee5f7c93e7a2a29e837ed37dfb11b67
Ifeanyitg <onuohathankgod22@gmail.com>
2025-06-08 01:16:56 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
-// Add your route handlers here
+const express = require('express');
+const router = express.Router();
+
+// Example route
+router.get('/', (req, res) => {
+  res.send('API is working!');
+});
+
+module.exports = router;
+
